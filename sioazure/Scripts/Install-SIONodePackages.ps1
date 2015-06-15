60274bfcdd17bda360cdd9696594e18f0f9eac9b
Brandon Kvarda <bjkvarda@gmail.com>
2015-06-15 07:39:30 -0700

    Removing some node pkg elements

--- sioazure/Scripts/Install-SIONodePackages.ps1
+++ sioazure/Scripts/Install-SIONodePackages.ps1
@@ -8,62 +8,39 @@ $onegeturl = "http://oneget.org/install-oneget.exe"
 $siourl = "http://bkvarda.blob.core.windows.net/sioazure/scaleio.zip"
 $pipurl = "https://bootstrap.pypa.io/get-pip.py"
 $pycompileurl = "http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
+$pythonurl = "http://bkvarda.blob.core.windows.net/sioazure/python-2.7.5.amd64.msi"
+$pywin32url = "http://bkvarda.blob.core.windows.net/sioazure/pywin32-216.0.win32-py2.7.msi"
 $rootdestination = "C:\scaleio"
 
-#Give use unlimited power muahahah
-Set-ExecutionPolicy Unrestricted
 
 #Disable all of the firewalls! (this makes things easier)
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Set-NetFirewallProfile -Profile Public,Private -Enabled False
 
-#Create the directory
+#Set up our data drive
+$disk = Get-Disk | Where partitionstyle -EQ 'raw'
+$disk | Initialize-Disk -PartitionStyle MBR
+$disk | New-Partition -AssignDriveLetter -UseMaximumSize
+
+
+#Create the working directory
 Write-Host "Creating $rootdestination"
 New-Item -Force -ItemType directory -Path $rootdestination
 
-#Download C++ Compiler for Python
-Write-Host "Downloading C++ Compiler for Python"
-$pycompiledestination = $rootdestination + "\python-compile.msi"
-Invoke-WebRequest $pycompileurl -Outfile $pycompiledestination
-
-#Install Compiler
-Write-Host "Installing C++ Compiler for Python"
-Start-Process $pycompiledestination /quiet -Wait
-
-#Download OneGet
-Write-Host "Downloading OneGet"
-$onegetdestination = $rootdestination + "\oneget.exe"
-Invoke-WebRequest $onegeturl -Outfile $onegetdestination
-
-#Install OneGet
-Write-Host "Installing OneGet"
-Start-Process $onegetdestination /s -Wait
+#Download Python
+$pythondestination = $rootdestination + "\python.msi"
+Invoke-WebRequest $pythonurl -OutFile $pythondestination
 
 #Install Python 2.7.5
 Write-Host "Installing Python"
-Install-Package -Name python -RequiredVersion 2.7.5 -Force
-Get-Package -Name python
+Start-Process -FilePath msiexec.exe -ArgumentList "/i $pythondestination /passive" -Wait
+
+#Download PyWin32
+$pywin32destination = $rootdestination + "\pywin32.msi" 
+Invoke-WebRequest $pywin32url -OutFile $pywin32destination
 
 #Install PyWin32
-Install-Package -Name pywin32 -Force
-Get-Package -Name pywin32
-
-#Downloading Pip
-$pipdestination = $rootdestination + "\get-pip.py"
-Invoke-WebRequest $pipurl -Outfile $pipdestination
-
-#Installing Pip
-python $pipdestination
-
-#Install Other Python Modules
-
-#Install Paramiko
-python -m pip install paramiko
-
-#Install Crypto
-python -m pip install pycrypto
-
-#Install WMI
-python -m pip install WMI
+Write-Host "Installing PyWin"
+Start-Process -FilePath msiexec.exe -ArgumentList "/i $pywin32destination /passive" -Wait
 
 Write-Host "All Done!"
 
