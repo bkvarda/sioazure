fcf1acc08a54d32f3f49bb19f9062298fb23399c
bkvarda <bjkvarda@gmail.com>
2015-06-15 10:28:51 -0700

    Added remote execution to mgmt, changed manifest dependency order

--- sioazure/Scripts/Install-SIOMgmtPackages.ps1
+++ sioazure/Scripts/Install-SIOMgmtPackages.ps1
@@ -6,6 +6,7 @@
 
 $onegeturl = "http://oneget.org/install-oneget.exe"
 $siourl = "http://bkvarda.blob.core.windows.net/sioazure/scaleio.zip"
+$nodescripturl = "http://bkvarda.blob.core.windows.net/sioazure/Install-SIONodePackages.ps1"
 $rootdestination = "C:\scaleio"
 
 #Give use unlimited power muahahah
@@ -14,6 +15,11 @@ Set-ExecutionPolicy Unrestricted
 #Create the directory
 Write-Host "Creating C:\scaleio..."
 New-Item -Force -ItemType directory -Path $rootdestination
+
+#Download Node script
+Write-Host "Downloading Node Script"
+$nodescriptdestination = $rootdestination + "\Install-SIONodePackages.ps1"
+Invoke-WebRequest $nodescripturl -Outfile $nodescriptdestination
 
 #Download OneGet
 Write-Host "Downloading OneGet"
@@ -45,6 +51,11 @@ Start-Process "$rootdestination\ScaleIO_1.32_GUI_for_Windows_Download\EMC-ScaleI
 #Install 64-bit version of SIO Gateway
 Write-Host "Intalling SIO Gateway"
 msiexec.exe /i "$rootdestination\ScaleIO_1.32_Gateway_for_Windows_Download\EMC-ScaleIO-gateway-1.32-402.1-x64.msi" /quiet  GATEWAY_ADMIN_PASSWORD='Password123' GATEWAY_ADMIN_PWD_CNFRM='Password123'
+
+#Installing Python packages on MDMs
+Write-Host "Installing Python on MDMs"
+Invoke-Command -ComputerName 10.0.0.5 -FilePath $nodescriptdestination
+Invoke-Command -ComputerName 10.0.0.6 -FilePath $nodescriptdestination
 
 #Build a CSV payload for creation of cluster
 
--- sioazure/Scripts/Install-SIONodePackages.ps1
+++ sioazure/Scripts/Install-SIONodePackages.ps1
@@ -10,6 +10,7 @@ $pipurl = "https://bootstrap.pypa.io/get-pip.py"
 $pycompileurl = "http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
 $pythonurl = "http://bkvarda.blob.core.windows.net/sioazure/python-2.7.5.amd64.msi"
 $pywin32url = "http://bkvarda.blob.core.windows.net/sioazure/pywin32-216.0.win32-py2.7.msi"
+$nodescripturl = "http://bkvarda.blob.core.windows.net/sioazure/Install-SIONodePackages.ps1"
 $rootdestination = "C:\scaleio"
 
 
@@ -41,6 +42,11 @@ Invoke-WebRequest $pywin32url -OutFile $pywin32destination
 #Install PyWin32
 Write-Host "Installing PyWin"
 Start-Process -FilePath msiexec.exe -ArgumentList "/i $pywin32destination /passive" -Wait
+
+#Download Node script
+Write-Host "Downloading Node Script"
+$nodescriptdestination = $rootdestination + "\Install-SIONodePackages.ps1"
+Invoke-WebRequest $nodescripturl -Outfile $nodescriptdestination
 
 Write-Host "All Done!"
 
